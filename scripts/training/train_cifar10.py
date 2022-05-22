f53fe83b1d0e9abf3bac83092e7f92b976fac4d9
Brian Weston <brianweston@MBP.local>
2022-05-21 22:29:10 -0700

    Added standard image dataloader and train cifar10 script

---
+++ scripts/training/train_cifar10.py
@@ -0,0 +1,121 @@
+import numpy as np
+import os
+import sys
+import cv2
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
+from torch.utils.data.sampler import SubsetRandomSampler
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import random
+
+# unfortunately this is required to use relative imports
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(
+                os.path.dirname(SCRIPT_DIR)))
+
+from scripts.models.model_conv_ae import Conv_AE
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# convert data to torch.FloatTensor
+transform = transforms.ToTensor()
+
+# load the training and test datasets
+train_data = datasets.CIFAR10(root='data', train=True,
+                                   download=True, transform=transform)
+test_data = datasets.CIFAR10(root='data', train=False,
+                                  download=True, transform=transform)
+
+# how many samples per batch to load
+batch_size = 20
+num_workers = 0
+
+# prepare data loaders
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, num_workers=num_workers)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, num_workers=num_workers)
+
+# helper function to un-normalize and display an image
+def imshow(img):
+    img = img / 2 + 0.5  # unnormalize
+    plt.imshow(np.transpose(img, (1, 2, 0)))  # convert from Tensor image
+    
+# specify the image classes
+classes = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck']
+
+# import convolutional autoencoder
+model = Conv_AE(1000)
+print(model)
+
+# prints number of parameters
+pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+print(f"Number of trainable parameters: {pytorch_total_params} ")
+
+# specify loss function
+criterion = nn.BCELoss() #BCE vs MSE
+
+# specify loss function
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+# number of epochs to train the model
+n_epochs = 10
+
+for epoch in range(1, n_epochs+1):
+    # monitor training loss
+    train_loss = 0.0
+    
+    ###################
+    # train the model #
+    ###################
+    for data in train_loader:
+        # _ stands in for labels, here
+        # no need to flatten images
+        images, _ = data
+        # clear the gradients of all optimized variables
+        optimizer.zero_grad()
+        # forward pass: compute predicted outputs by passing inputs to the model
+        outputs = model(images)
+        # calculate the loss
+        loss = criterion(outputs, images)
+        # backward pass: compute gradient of the loss with respect to model parameters
+        loss.backward()
+        # perform a single optimization step (parameter update)
+        optimizer.step()
+        # update running training loss
+        train_loss += loss.item()*images.size(0)
+            
+    # print avg training statistics 
+    train_loss = train_loss/len(train_loader)
+    print('Epoch: {} \tTraining Loss: {:.6f}'.format(
+        epoch, 
+        train_loss
+        ))
+
+# get sample outputs
+output = model(images)
+
+# output is resized into a batch of iages
+output = output.view(batch_size, 3, 32, 32)
+# use detach when it's an output that requires_grad
+output = output.detach().numpy()
+
+
+# plot the first ten input images and then reconstructed images
+fig, axes = plt.subplots(nrows=2, ncols=10, sharex=True, sharey=True, figsize=(24,4))
+for idx in np.arange(20):
+    ax = fig.add_subplot(2, 20/2, idx+1, xticks=[], yticks=[])
+    imshow(output[idx])
+    fig.savefig('reconstructed_BCE.png')
+    ax.set_title(classes[labels[idx]])
+    
+# plot the first ten input images and then reconstructed images
+fig, axes = plt.subplots(nrows=2, ncols=10, sharex=True, sharey=True, figsize=(24,4))
+for idx in np.arange(20):
+    ax = fig.add_subplot(2, 20/2, idx+1, xticks=[], yticks=[])
+    imshow(images[idx])
+    fig.savefig('original.png')
+    ax.set_title(classes[labels[idx]])
